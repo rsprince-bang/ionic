74386e9e2e228e76329367f858c2855ebad22f5f
Stoyan Raychev <stoyan.raychev@vpxsports.com>
2019-03-19 13:47:31 -0400

    import in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
-//import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions/ngx';
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions/ngx';
 
 
 @NgModule({
@@ -25,8 +25,8 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-    //NativePageTransitions
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    NativePageTransitions
   ],
   bootstrap: [AppComponent]
 })
