49cce0f0c29db505cbd823d077abf6225931355b
Stoyan Raychev <stoyan.raychev@vpxsports.com>
2019-03-19 11:50:57 -0400

    slide with less time and iclude in page ts only

--- src/app/user_pages/tab2/tab2.page.ts
+++ src/app/user_pages/tab2/tab2.page.ts
@@ -16,13 +16,13 @@ export class Tab2Page {
 
     let options: NativeTransitionOptions = {
       direction: 'left',
-      duration: 500,
-      slowdownfactor: 3,
-      slidePixels: 20,
-      iosdelay: 100,
-      androiddelay: 150,
-      fixedPixelsTop: 0,
-      fixedPixelsBottom: 60
+      duration: 400,
+      slowdownfactor: -1,
+      //slidePixels: 20,
+      //iosdelay: 100,
+      //androiddelay: 150,
+      //fixedPixelsTop: 0,
+      //fixedPixelsBottom: 60
     }
     this.nativePageTransitions.slide(options);
 
