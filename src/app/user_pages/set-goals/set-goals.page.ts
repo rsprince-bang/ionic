e7e2f60f9cc67a76b6af7978263415c2073b6a8a
Stoyan Raychev <stoyan.raychev@vpxsports.com>
2020-09-24 17:04:36 -0400

    done set goals

--- src/app/user_pages/set-goals/set-goals.page.ts
+++ src/app/user_pages/set-goals/set-goals.page.ts
@@ -67,37 +67,13 @@ export class SetGoalsPage implements OnInit {
     )
     .subscribe(
       (result) => {
-        console.log(result);
 
-        // if( this.userInfo.error ){
-        //   this.presentToastWithOptions(this.userInfo.error);
-        // }
-        // else if(this.userInfo.success){
-        //   localStorage.setItem("token", this.userInfo.success.token);
-        //   localStorage.setItem("user_id", this.userInfo.success.user_id);
-        //   this.events.publish("user logged in", 1111, 2222); //test passsing args
-
-        //   if( this.userInfo.success.user.goals.length == 0 ){
-        //     //user never filled out goals
-        //     //this.router.navigateByUrl("/set-goals");
-        //     this.router.navigateByUrl("/welcome");
-        //   }
-        //   else if( this.userInfo.success.user.measurements.length == 0 ){
-        //     //user never filled out measurements
-        //     this.router.navigateByUrl("/enter-measurements");
-        //   }
-        //   else{
-        //     //localStorage.setItem('diet_start_date', JSON.stringify(result.success.diet_start_date));
-        //     //localStorage.setItem('dailyCaloriesIntake', this.userInfo.success.dailyCaloriesIntake);
-        //     //localStorage.setItem("currentCaloriesIntake", result.success.currentCaloriesIntake);
-        //     //localStorage.setItem("lastFeedback", result.success.lastFeedback);
-        //     //localStorage.setItem('diet_plan_length', result.success.plan_length);
-        //     this.router.navigateByUrl("tabs/home");
-        //   }
-        // }
-        // else{
-        //   this.presentToastWithOptions("Something went wrong, please try again later.");
-        // }
+        if( result.error ){
+          this.myAPI.handleMyAPIError(result.error);
+        }
+        else{
+          this.router.navigateByUrl("/enter-measurements");
+        }
       }
     );
   }
