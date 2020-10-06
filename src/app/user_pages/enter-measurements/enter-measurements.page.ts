4c5a57ad8eca732a2b716661886e09db8a5568ca
Richard Sprince <Richard.Sprince@bangenergy.com>
2020-10-06 09:52:18 -0400

    Edits enter-measurements.

--- src/app/user_pages/enter-measurements/enter-measurements.page.ts
+++ src/app/user_pages/enter-measurements/enter-measurements.page.ts
@@ -49,19 +49,7 @@ export class EnterMeasurementsPage implements OnInit {
 		private navCtrl: NavController,
 		private foodSuggestionsService: FoodSuggestionsService, 
 		private globalServices: GlobalServicesService
-	) {
-
-    this.route.queryParams.subscribe(params => {
-      if ( this.router.getCurrentNavigation().extras && this.router.getCurrentNavigation().extras.state) {
-        this.action = this.router.getCurrentNavigation().extras.state.action; // has value of "update" because it is coming from profile page
-                                                                              //or has value of "confirm" if coming from reset diet redirect
-        this.currentUserMeasurements = this.router.getCurrentNavigation().extras.state.userMeasurements;
-        this.currentUserMeasurements.heightInches = "" + Math.floor(this.router.getCurrentNavigation().extras.state.userMeasurements.height_inches/12);
-        this.currentUserMeasurements.plan = this.foodSuggestionsService.getDietPlanWeeks().toString();
-      }
-    });
-    
-  }
+	) { }
 
   ngOnInit() {
     this.measurementsForm = this.formBuilder.group({
