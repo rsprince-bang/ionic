b0b93dbabbaafddd8b0926bd9c48889f56e7d40f
Richard Sprince <Richard.Sprince@bangenergy.com>
2020-10-07 10:47:36 -0400

    Tweaks layout.

--- src/app/user_pages/add-event-modal/add-event-modal.page.ts
+++ src/app/user_pages/add-event-modal/add-event-modal.page.ts
@@ -8,14 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-event-modal.page.scss'],
 })
 export class AddEventModalPage implements OnInit {
-  todayWeight: number;
-  weightRange: number[] = [];
-  todayBMI: string;
-  todayFat: string;
-  todayDate = new Date(); 
-  bmiRange = ['below 18', '19 - 24', '25 - 29', '30 - 34', '35 - 39', 'above 40'];
-  fatRange = ['below 18', '19 - 24', '25 - 29', '30 - 34', '35 - 39', 'above 40'];
-
+  searchTerm: string;
   constructor(public modalController: ModalController,public router:Router) { }
 
   ngOnInit() {
@@ -26,27 +19,10 @@ export class AddEventModalPage implements OnInit {
     this.modalController.dismiss();
   }
 
-  generateWeight(start, end) {
-    for(let i=start; i <= end; i+= 5) {
-      this.weightRange.push(i);
-    }
-    return this.weightRange;
-  }
 
-  addMeal() {
-    console.log("Add Meal");
-    this.modalController.dismiss();
-    this.router.navigateByUrl('/home-add');
-  }
-
-  addWorkout() {
-    console.log("Add Workout");
-  }
 
   sendData() {
-    console.log(this.todayWeight);
-    console.log(this.todayBMI);
-    console.log(this.todayFat);
+    console.log(this.searchTerm);
   }
 
 }
