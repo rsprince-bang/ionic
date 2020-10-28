26a59e90ab62a6c9bb33e51f993bb552d42f9419
Richard Sprince <Richard.Sprince@bangenergy.com>
2020-10-28 13:51:20 -0400

    Tweaks a comment.

--- src/app/user_pages/user-info/user-info.page.ts
+++ src/app/user_pages/user-info/user-info.page.ts
@@ -37,15 +37,61 @@ export class UserInfoPage implements OnInit {
       email: [this.userInfo.email, [Validators.required, Validators.email]],
 			password: [this.userInfo.password, [Validators.required, Validators.minLength(8)]],
 			newpassword: [this.userInfo.newpassword, [Validators.required, Validators.minLength(8)]],
-    });
+		});
+		// get current user info
+		this.getCurrentUserInfo();
   }
 
+	// Dismisses modal
   dismiss() {
     this.modalController.dismiss();
 	}
 	
+	// Saves edits to user info
 	onSave() {
 		console.log(this.userInfoForm.value);
+	}
+
+	// gets current user info from the endpoint
+	getCurrentUserInfo() {
+		console.log("Gettin user info...")
+		this.myAPI.makeAPIcall(
+			"user-info",
+			{"action": "loadUserInfo"},
+			true
+		)
+		.subscribe(
+			response => {
+				if( response.error ){
+					this.myAPI.handleMyAPIError(response.error);
+				} else {
+					// set response values to local values
+					this.userInfo.first = response.first_name;
+					this.userInfo.last = response.last_name;
+					this.userInfo.email = response.email;
+					this.userInfo.password = response.password;
+				}
+			},
+			error => console.log(error)
+		)
+	}
+
+	// Updates user info
+	updatetUserInfo() {
+		console.log("Gettin user info...")
+		this.myAPI.makeAPIcall(
+			"user-info",
+			{"action": "saveUserInfo"},
+			true
+		)
+		.subscribe(
+			response => {
+				if( response.error ){
+					this.myAPI.handleMyAPIError(response.error);
+				}
+			},
+			error => console.log(error)
+		)
 	}
 
 }
