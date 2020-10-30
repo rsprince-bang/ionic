bbc4943f436ce472caf14b65668eed88fc9d49d1
Richard Sprince <Richard.Sprince@bangenergy.com>
2020-10-30 16:00:31 -0400

    Adds data access for sleep modal.

--- src/app/user_pages/add-sleep-modal/add-sleep-modal.page.ts
+++ src/app/user_pages/add-sleep-modal/add-sleep-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { ApiCallService } from 'src/app/services/api-call.service';
 
 @Component({
   selector: 'app-add-sleep-modal',
@@ -8,8 +9,12 @@ import { ModalController } from '@ionic/angular';
 })
 export class AddSleepModalPage implements OnInit {
   sleepAmount: number = 0;
+	sleep = {intake: 15, recommended: 60 };
 
-  constructor(public modalController: ModalController) { }
+  constructor(
+    public modalController: ModalController,
+    private myAPI: ApiCallService
+  ) { }
 
   ngOnInit() {
   }
@@ -27,7 +32,29 @@ export class AddSleepModalPage implements OnInit {
 	}
 
 	sendData() {
-		console.log(this.sleepAmount);
+    console.log(this.sleepAmount);
+    // this.updateSleep();
+  }
+  
+  	// update water intake
+	updateSleep() {
+		this.myAPI.makeAPIcall(
+			"sleep",
+			{
+				"action": "saveSleep",
+				"waterAmount": this.sleepAmount
+			},
+			true
+		)
+		.subscribe(
+			response => {
+				// handle error
+				if( response.error ){
+					this.myAPI.handleMyAPIError(response.error);
+				}
+			},
+			error => console.log(error)
+		)
 	}
 
 }
