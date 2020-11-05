5d3940e73868eb80f3276b6c063f5723549fa8d5
Richard Sprince <Richard.Sprince@bangenergy.com>
2020-11-05 15:33:17 -0500

    Stems out Add Meals search API call, adds dynamic values to Meals template.

--- src/app/user_pages/add-event-modal/add-event-modal.page.ts
+++ src/app/user_pages/add-event-modal/add-event-modal.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { ApiCallService } from 'src/app/services/api-call.service';
 
 @Component({
   selector: 'app-add-event-modal',
@@ -9,7 +10,13 @@ import { Router } from '@angular/router';
 })
 export class AddEventModalPage implements OnInit {
   searchTerm: string;
-  constructor(public modalController: ModalController,public router:Router) { }
+  meals = {};
+  
+  constructor(
+    public modalController: ModalController,
+    public router:Router,
+    private myAPI: ApiCallService
+  ) { }
 
   ngOnInit() {
   }
@@ -18,7 +25,33 @@ export class AddEventModalPage implements OnInit {
     this.modalController.dismiss();
   }
 
+  searchMeals() {
+    console.log("Search Meals", this.searchTerm);
 
+    this.myAPI.makeAPIcall(
+			"searchMeals",
+			{
+        "action": "sendSearchTerm",
+        "searchTerm": this.searchTerm
+			},
+			true
+		)
+		.subscribe(
+			response => {
+				// handle error
+				if( response.error ){
+					this.myAPI.handleMyAPIError(response.error);
+				} 
+				// successful response
+				else {
+					let data = response.success;
+					this.meals = data.meals;
+					console.log();
+				}
+			},
+			error => console.log(error)
+		)
+  }
 
   sendData() {
     console.log(this.searchTerm);
